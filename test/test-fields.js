b359d4afe408507db28f89d6cab64d35440e4c8b
Adam Hull <adam@hmlad.com>
2013-03-13 00:56:30 -0700

    Spilt out attached field tests and always show error_msg

--- test/test-fields.js
+++ test/test-fields.js
@@ -18,14 +18,23 @@ var testField = function (field) {
         // field has an attach method
         test.ok(f.attach instanceof Function);
 
+        // always includes error message wrapper in HTML, even when there's no error
+        test.ok(/class.*error_msg/.test(f.toHTML()));
+        test.done();
+    };
+
+    exports[field + ' attached'] = function (test) {
+        var f = fields[field]({
+            label: 'test label',
+            name: 'test'
+        });
         var fieldElement = elementFromString(f.toHTML());
         var widget = widgets.forInput(fieldElement.querySelector('[name=test]'));
         var attached = f.attach(widget);
 
-        // attached fields have showError and clearError methods
+        // has methods to show and clear errors
         test.ok(attached.showError instanceof Function);
         test.ok(attached.clearError instanceof Function);
-
         test.done();
     };
 };
