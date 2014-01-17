ae691b2cea386364dbb2bfd6d70ab431ff6a7d2b
Tim Robinson <tim@timjrobinson>
2014-01-17 16:24:33 +0000

    Upgraded forms to latest release (0.3.0) and fixed broken tests.

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,9 @@
 module.exports = function (config) {
-    config = {
+    config.set({
+        frameworks: ['nodeunit'],
         files:  [
-            'node_modules/karma-nodeunit/lib/nodeunit.js',
-            'node_modules/karma-nodeunit/lib/adapter.js',
             'test/built/*.js'
         ],
-        browsers: ['Chrome'],
         autoWatch: false
-    };
+    });
 };
