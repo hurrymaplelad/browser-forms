2a0836fd7972981d2e9e1759ba882117904156b5
Tim Robinson <tim@timjrobinson>
2014-01-19 13:08:53 +0000

    Added support for forms that have multiple sections. Fixed errors when a field is initialized but not displayed on the page.

--- test/test-forms.js
+++ test/test-forms.js
@@ -9,6 +9,16 @@ var form = forms.create({
     password: fields.password({required: true})
 });
 
+var multiSectionForm = forms.create({
+    userDetails: {
+        username: fields.string({required: true}),
+        password: fields.password({required: true})
+    },
+    address: {
+        street: fields.string({required: true})
+    }
+});
+
 module.exports = {
     'are attachable': function (test) {
         test.ok(form.attach instanceof Function);
@@ -28,4 +38,18 @@ module.exports = {
             test.done();
         }
     }),
+
+    'multi form attach': testCase({
+        setUp: function (done) {
+            var formEl = document.createElement('form');
+            formEl.innerHTML = multiSectionForm.toHTML();
+            this.attachedForm = multiSectionForm.attach(formEl);
+            done();
+        },
+
+        'work': function (test) {
+            test.ok(this.attachedForm);
+            test.done();
+        }
+    })
 };
